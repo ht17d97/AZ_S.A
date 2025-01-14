d48ba99e6a5b1292e52acd813f6c296fb0a5043c
ht17d97 <63954538+ht17d97@users.noreply.github.com>
2025-01-14 22:04:19 +0530

    Create provider.tf

---
+++ provider.tf
@@ -0,0 +1,8 @@
+provider "azurerm" {
+  subscription_id = "8be726a9-c720-483e-a2d6-45d8342f8d72"
+  tenant_id       = "884fc288-3f69-4a7c-91a2-ee1a25f916e2"
+  client_id       = "f1beaaea-c41a-4ff6-88ef-1f2c923f3ebb"
+  client_secret   = "var.client_secret"
+  features {
+  }
+}
