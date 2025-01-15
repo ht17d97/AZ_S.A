26be91c7c9976dbce26861d11f9fb7a80cbe3c32
ht17d97 <63954538+ht17d97@users.noreply.github.com>
2025-01-15 22:17:42 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,17 +3,26 @@ data "azurerm_resource_group" "example" {
 }
 data "azurerm_virtual_network" "existing_vnet" {
   name                = "example-network"
-  resource_group_name = "Terraform-RG"
+  resource_group_name = azurerm_resource_group.example.name
 }
-data "azurerm_subnet" "existing_subnet" {
-  name                 = "internal"
-  virtual_network_name = data.azurerm_virtual_network.existing_vnet.name
-  resource_group_name  = "Terraform-RG"
+resource "azurerm_subnet_service_endpoint_storage_policy" "example" {
+  name                = "SA-subnet-policy"
+  resource_group_name = azurerm_resource_group.example.name
+  location            = "South India"
+  definition {
+    name        = "MS.Storage"
+    description = "testing"
+    service     = "Microsoft.Storage"
+    service_resources = [
+      azurerm_resource_group.example.id,
+      azurerm_storage_account.example.id
+    ]
+  }
 }
 resource "azurerm_storage_account" "storage" {
   name                     = "azhimstoaccountaqaqaq" # Must be globally unique
   resource_group_name      = "Terraform-RG"
-  location                 = "South India "
+  location                 = "South India"
   account_tier             = "Standard"
   account_replication_type = "LRS"
 
