71dfab67ba488690eb4ec9e3d3b849a1cf9e7e23
ht17d97 <63954538+ht17d97@users.noreply.github.com>
2025-01-15 19:57:56 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,8 +1,7 @@
 provider "azurerm" {
-  subscription_id = "8be726a9-c720-483e-a2d6-45d8342f8d72"
-  tenant_id       = "884fc288-3f69-4a7c-91a2-ee1a25f916e2"
-  client_id       = "f1beaaea-c41a-4ff6-88ef-1f2c923f3ebb"
-  client_secret   = "var.client_secret"
-  features {
-  }
+  subscription_id = var.subscription_id
+  tenant_id       = var.tenant_id
+  client_id       = var.client_id
+  client_secret   = var.client_secret
+  features {}
 }
